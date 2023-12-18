22153d2485d01b8de773dcd9f45caf2ed38a4e0c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-18 07:31:29 -0500

    Icons and embedding into components

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,6 +3,9 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Container from "@mui/material/Container";
+import AcUnitOutlinedIcon from "@mui/icons-material/AcUnitOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
 
 export default function Create() {
   return (
@@ -35,8 +38,25 @@ export default function Create() {
         <ButtonGroup color="secondary" variant="contained">
           <Button>One</Button>
           <Button>Two</Button>
-          <Button>Three</Button>
+          <Button
+            startIcon={<SendOutlinedIcon />}
+            endIcon={<MailOutlinedIcon />}
+          >
+            Three
+          </Button>
         </ButtonGroup>
+      </Container>
+      <Container>
+        <AcUnitOutlinedIcon />
+      </Container>
+      <Container>
+        <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      </Container>
+      <Container>
+        <AcUnitOutlinedIcon color="action" fontSize="small" />
+      </Container>
+      <Container>
+        <AcUnitOutlinedIcon color="error" fontSize="small" />
       </Container>
     </Container>
   );
