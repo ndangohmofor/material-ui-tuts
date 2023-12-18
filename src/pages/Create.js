16e0f4ee7242dcf1a126e0aee854d821aee05911
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-18 07:04:45 -0500

    Typography component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        component={"h2"}
+        gutterBottom
+        color={"textSecondary"}
+      >
+        Create a New Note
+      </Typography>
     </div>
-  )
+  );
 }
