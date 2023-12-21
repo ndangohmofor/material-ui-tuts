742bc25605e40db13018977770c3e92a84be6bf8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 11:22:57 -0500

    Fixed the Appbar and added a margin between app bar and the cards

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Create from "./pages/Create";
 import TourCard from "./components/TourCard";
 import Grid from "@mui/material/Grid";
 import "./App.css";
+import SearchAppBar from "./components/Appbar";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Container>
+          <SearchAppBar />
+          <Container sx={{ marginY: 5 }}>
             <Grid container spacing={5}>
               <TourCard />
               <TourCard />
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -51,7 +51,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function AppBar() {
+export default function SearchAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
