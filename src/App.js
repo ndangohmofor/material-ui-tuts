eb294a379deaf5fcf73d3e00d3dad913a649e3fc
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 11:28:49 -0500

    Added the data to the page dynamically

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import TourCard from "./components/TourCard";
 import Grid from "@mui/material/Grid";
 import "./App.css";
 import SearchAppBar from "./components/Appbar";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
@@ -13,12 +15,22 @@ function App() {
         <Route exact path="/">
           <SearchAppBar />
           <Container sx={{ marginY: 5 }}>
-            <Grid container spacing={5}>
+            {/* <Grid container spacing={5}>
               <TourCard />
               <TourCard />
               <TourCard />
               <TourCard />
-            </Grid>
+            </Grid> */}
+            {cities.map((city) => (
+              <Typography
+                variant="h4"
+                component={"h2"}
+                marginTop={5}
+                marginBottom={3}
+              >
+                Top {city.name} Tours
+              </Typography>
+            ))}
           </Container>
         </Route>
         <Route path="/create">
