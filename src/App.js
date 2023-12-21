bcc222af46619f60c3f0403354ddaca4045d895b
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 12:09:08 -0500

    Moved the home component from the app to the home component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Container from "@mui/material/Container";
 import Create from "./pages/Create";
-import TourCard from "./components/TourCard";
-import Grid from "@mui/material/Grid";
 import "./App.css";
-import SearchAppBar from "./components/Appbar";
-import cities from "./data.json";
-import { Typography } from "@mui/material";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <SearchAppBar />
-          <Container sx={{ marginY: 5 }}>
-            {cities.map((city) => (
-              <>
-                <Typography
-                  variant="h4"
-                  component={"h2"}
-                  marginTop={5}
-                  marginBottom={3}
-                >
-                  Top {city.name} Tours
-                </Typography>
-                <Grid container spacing={5}>
-                  {city.tours.map((tour, idx) => (
-                    <TourCard tour={tour} key={idx} />
-                  ))}
-                </Grid>
-              </>
-            ))}
-          </Container>
+          <Home />
         </Route>
         <Route path="/create">
           <Create />
