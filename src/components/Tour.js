25d18444201a42bab5fdcc983d53f9fbcd325192
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 14:17:12 -0500

    Start populating the Tour component with aspects of each tour

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 const Tour = (props) => {
-  return <div>hello</div>;
+  return (
+    <Container
+      sx={{
+        width: 900,
+      }}
+    >
+      <Typography variant="h3" component={"h1"} marginTop={3}>
+        Explore the World in Vegas
+      </Typography>
+    </Container>
+  );
 };
 
 export default Tour;
