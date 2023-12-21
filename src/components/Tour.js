8bd376ba7b0dff81ae3d839790234b91104bbe19
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 14:52:11 -0500

    Imported the group of images into the Tour component to display the images from a tour

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ImageCollage from "./ImageCollage";
 
 const Tour = (props) => {
   return (
@@ -12,6 +14,29 @@ const Tour = (props) => {
       <Typography variant="h3" component={"h1"} marginTop={3}>
         Explore the World in Vegas
       </Typography>
+      <Box
+        marginTop={3}
+        sx={{
+          display: "flex",
+        }}
+      >
+        <img src="/images/falls-4.jpg" alt="some image of vegas" height={325} />
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography variant="h6" component={"h4"} marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" component={"p"} marginTop={3}>
+          Ea dolore minim aute culpa proident ullamco tempor aliqua duis.
+          Laboris aute fugiat irure tempor minim. Qui veniam ea ullamco nisi ut
+          cupidatat eiusmod ipsum pariatur magna reprehenderit velit.
+          Consectetur qui eiusmod non esse non. Consectetur nulla laborum enim
+          labore dolore. Fugiat in qui eiusmod adipisicing cillum irure sunt
+          dolore pariatur cillum non laborum laborum tempor. Ullamco nostrud ad
+          nulla nisi esse et in consequat duis quis commodo deserunt anim.
+        </Typography>
+      </Box>
     </Container>
   );
 };
