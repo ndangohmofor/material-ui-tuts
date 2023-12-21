43c2b03993769114cb365de2a502723262da63fc
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 12:18:24 -0500

    Created another route for viewing a tour

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Create from "./pages/Create";
 import "./App.css";
 import Home from "./pages/Home";
+import SearchAppBar from "./components/Appbar";
+import Tour from "./components/Tour";
 
 function App() {
   return (
     <Router>
+      <SearchAppBar />
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/create">
-          <Create />
+        <Route path="/:id">
+          <Tour />
         </Route>
       </Switch>
     </Router>
