f16a4d861e1e50f69b1c2404847f6c9f8ce734c1
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 11:36:43 -0500

    Updated the App so pass in data from data.json into the tour cards to display the different tours and their towns

--- src/App.js
+++ src/App.js
@@ -15,21 +15,22 @@ function App() {
         <Route exact path="/">
           <SearchAppBar />
           <Container sx={{ marginY: 5 }}>
-            {/* <Grid container spacing={5}>
-              <TourCard />
-              <TourCard />
-              <TourCard />
-              <TourCard />
-            </Grid> */}
             {cities.map((city) => (
-              <Typography
-                variant="h4"
-                component={"h2"}
-                marginTop={5}
-                marginBottom={3}
-              >
-                Top {city.name} Tours
-              </Typography>
+              <>
+                <Typography
+                  variant="h4"
+                  component={"h2"}
+                  marginTop={5}
+                  marginBottom={3}
+                >
+                  Top {city.name} Tours
+                </Typography>
+                <Grid container spacing={5}>
+                  {city.tours.map((tour, idx) => (
+                    <TourCard tour={tour} key={idx} />
+                  ))}
+                </Grid>
+              </>
             ))}
           </Container>
         </Route>
