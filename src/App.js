5816b34d1893764f2e0c75aadd53cb6f93ba974b
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-21 10:15:44 -0500

    Added the tourcard component and render to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Create from "./pages/Create";
+import TourCard from "./components/TourCard";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Notes />
+          <Container>
+            <Grid container spacing={5}>
+              <TourCard />
+              <TourCard />
+              <TourCard />
+              <TourCard />
+            </Grid>
+          </Container>
         </Route>
         <Route path="/create">
           <Create />
